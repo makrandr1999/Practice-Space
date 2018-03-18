ca1e7683ac3d00cc6d1e29cb3155d2c830f4005c
Gaurang484 <gaurangpatel484@gmail.com>
2018-03-18 08:34:04 +0530

    CHEGLOVE.java

---
+++ CHEGLOVE.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CHEGLOVE
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc. nextInt();
+		int i,j;
+		while(t-- > 0)
+		{
+			int n = sc. nextInt();
+			int[] finger = new int[n];
+			int[] sheath = new int[n];
+			boolean front = false;
+			boolean back = false;
+			boolean both = false;
+			//take input
+			for( i = 0; i < n; i++)
+			{
+				finger[i] = sc.nextInt();
+			}
+			for( i = 0; i < n; i++)
+			{
+				sheath[i] = sc.nextInt();
+			}
+			//main logic
+			for( i = 0; i < n; i++)
+			{
+				if(finger[i] <= sheath[i])
+					front = true;
+				else {front = false; break;}
+			}
+			for( i = 0, j = n-1; i < n; i++, j--)
+			{
+				if(finger[i] <= sheath[j])
+					back = true;
+				else {back = false; break;}
+			}
+			if(front && back)
+				both = true;
+
+			if(front && !both)
+				System.out.println("front");
+			else if(back && !both)
+					System.out.println("back");
+				else if (both)
+						System.out.println("both");	
+					else System.out.println("none");			
+		}		
+	}
+}
